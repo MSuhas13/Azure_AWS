b0140da00b19b49e9feb2d94834d32fb615f4d46
MSuhas13 <112408999+MSuhas13@users.noreply.github.com>
2023-10-16 19:32:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,11 @@
-variable "aws_region" {
-  type    = string
-  default = "us-west-2"
-}
-
-
 provider "aws" {
-    region = var.aws_region 
+    region = "ap-south-1"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-0f5ee92e2d63afc18" # ap-south-1
   instance_type = "t2.micro"  
-  tags = {
-      Name = "TF-Instance-iac"
-  }
-  lifecycle {
-    create_before_destroy = true
+   tags = {
+      Name = "TF-Server"
   }
 }
